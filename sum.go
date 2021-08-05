e1723521b0dfd67511ba36d76b22e37f7178074b
hdd2k <hank.lee.qed@gmail.com>
2021-08-04 22:19:01 -0700

    Add SumAllTails + test

--- sum.go
+++ sum.go
@@ -7,3 +7,26 @@ func Sum(nums []int) int {
 	}
 	return summed
 }
+
+func SumAll(arrs ...[]int) (sums []int) {
+	var arrResults []int
+	for _, numbers := range arrs {
+		arrResults = append(arrResults, Sum(numbers))
+	}
+	return arrResults
+}
+
+func SumAllTail(arrs ...[]int) (sumTails []int) {
+	var arrResults []int
+	for _, numbers := range arrs {
+		numsLen := len(numbers)
+		var tailNums []int
+		if numsLen == 0 {
+			tailNums = numbers
+		} else {
+			tailNums = numbers[1:]
+		}
+		arrResults = append(arrResults, Sum(tailNums))
+	}
+	return arrResults
+}
--- sum_test.go
+++ sum_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	t.Run("collection of any nums", func(t *testing.T) {
@@ -10,5 +13,42 @@ func TestSum(t *testing.T) {
 		if summed != expected {
 			t.Errorf("expected %d but got %d, given %v", expected, summed, numbers)
 		}
+	})
+}
+
+func TestSumAll(t *testing.T) {
+	sumAlled := SumAll(
+		[]int{1, 2, 4},
+		[]int{4, 5, 7},
+	)
+	expected := []int{7, 16}
+	if !reflect.DeepEqual(sumAlled, expected) {
+		t.Errorf("expected %v got %v", expected, sumAlled)
+	}
+}
+
+func TestSumAllTails(t *testing.T) {
+	checkSums := func(tb testing.TB, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("expected %v got %v", want, got)
+		}
+	}
+
+	t.Run("SumAllTail with 1+ len arrs", func(t *testing.T) {
+		sumAllTailed := SumAllTail(
+			[]int{1, 2, 4},
+			[]int{4, 5, 7},
+		)
+		expected := []int{6, 12}
+		checkSums(t, sumAllTailed, expected)
+	})
+	t.Run("SumAllTail with 0 len arrs", func(t *testing.T) {
+		sumAllTailed := SumAllTail(
+			[]int{},
+			[]int{},
+		)
+		expected := []int{0, 0}
+		checkSums(t, sumAllTailed, expected)
 	})
 }
