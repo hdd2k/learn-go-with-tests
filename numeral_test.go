28ae07a1010beb40ff29166cdf663ad3caf9416d
hdd2k <hank.lee.qed@gmail.com>
2021-09-17 09:44:21 -0700

    Add numerals basic test + min passing code

---
+++ numeral_test.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestNumeral(t *testing.T) {
+	got := ConvertToRoman(1)
+	want := "I"
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
