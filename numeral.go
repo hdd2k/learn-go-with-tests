2dc3767c951f59a5c14e1138c7a1803765bedef6
hdd2k <hank.lee.qed@gmail.com>
2021-09-17 10:04:23 -0700

    Refactor numeric conversion special case 4 into string append loop

--- numeral.go
+++ numeral.go
@@ -5,12 +5,12 @@ import (
 )
 
 func ConvertToRoman(arabic int) string {
-	if arabic == 4 {
-		return "IV"
-	}
-
 	var result strings.Builder
-	for i := 0; i < arabic; i++ {
+	for i := arabic; i > 0; i-- {
+		if arabic == 4 {
+			result.WriteString("IV")
+			break
+		}
 		result.WriteString("I")
 	}
 	return result.String()
