eadb7e0f22474819deacaf4e086225b3e86f6339
hdd2k <hank.lee.qed@gmail.com>
2021-09-21 09:03:22 -0700

    Refactor numerals to extract switch pattern to OO

--- numeral.go
+++ numeral.go
@@ -4,23 +4,26 @@ import (
 	"strings"
 )
 
+type RomanNum struct {
+	Value  int
+	Symbol string
+}
+
+var RomanNums = []RomanNum{
+	{10, "X"},
+	{9, "IX"},
+	{5, "V"},
+	{4, "IV"},
+	{1, "I"},
+}
+
 func ConvertToRoman(arabic int) string {
 	var result strings.Builder
 
-	for arabic > 0 {
-		switch {
-		case arabic > 8:
-			result.WriteString("IX")
-			arabic -= 9
-		case arabic > 4:
-			result.WriteString("V")
-			arabic -= 5
-		case arabic > 3:
-			result.WriteString("IV")
-			arabic -= 4
-		default:
-			result.WriteString("I")
-			arabic -= 1
+	for _, numeral := range RomanNums {
+		for arabic >= numeral.Value {
+			result.WriteString(numeral.Symbol)
+			arabic -= numeral.Value
 		}
 	}
 	return result.String()
--- numeral_test.go
+++ numeral_test.go
@@ -11,51 +11,16 @@ func TestNumeral(t *testing.T) {
 		Arabic int
 		Want   string
 	}{
-		{
-			"1 converts to I",
-			1,
-			"I",
-		},
-		{
-			"2 converts to II",
-			2,
-			"II",
-		},
-		{
-			"3 converts to III",
-			3,
-			"III",
-		},
-		{
-			"4 converts to IV",
-			4,
-			"IV",
-		},
-		{
-			"5 converts to V",
-			5,
-			"V",
-		},
-		{
-			"6 converts to VI",
-			6,
-			"VI",
-		},
-		{
-			"7 converts to VII",
-			7,
-			"VII",
-		},
-		{
-			"8 converts to VIII",
-			8,
-			"VIII",
-		},
-		{
-			"9 converts to IX",
-			9,
-			"IX",
-		},
+		{"1 converts to I", 1, "I"},
+		{"2 converts to II", 2, "II"},
+		{"3 converts to III", 3, "III"},
+		{"4 converts to IV", 4, "IV"},
+		{"5 converts to V", 5, "V"},
+		{"6 converts to VI", 6, "VI"},
+		{"7 converts to VII", 7, "VII"},
+		{"8 converts to VIII", 8, "VIII"},
+		{"9 converts to IX", 9, "IX"},
+		{"10 converts to X", 10, "X"},
 	}
 
 	for _, c := range cases {
