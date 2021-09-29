45a893dda963363346449dc4cacabf199700169d
hdd2k <hank.lee.qed@gmail.com>
2021-09-29 08:51:08 -0700

    Add unit tests for math clock secondhand

--- math.go
+++ math.go
@@ -1,14 +1,35 @@
 package main
 
 import (
+	"math"
 	"time"
 )
 
 type Point struct {
-	X int
-	Y int
+	X float64
+	Y float64
 }
 
 func SecondHand(time time.Time) Point {
-	return Point{150, 150 - 90}
+	// angle from 12 o'clock (in degree) - minutes granularity
+	deg := (360 / 60) * (time.Minute())
+
+	// (in radian)
+	rad := (float64(2) * math.Pi) * (float64(deg) / 360.0)
+
+	// calculate offsets from center (note: 90 is the second hand length)
+	xOffset := math.Sin(rad) * 90
+	yOffset := math.Cos(rad) * 90
+
+	secondHandPoint := Point{
+		X: (150.0 + xOffset),
+		Y: (150.0 - yOffset),
+	}
+	return secondHandPoint
+
+	// return Point{150, 150 - 90}
+}
+
+func secondsInRadians(time time.Time) float64 {
+	return math.Pi
 }
--- math_test.go
+++ math_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"math"
 	"testing"
 	"time"
 )
@@ -10,7 +11,7 @@ import (
 // the minute hand is 80 long
 // the second hand is 90 long.
 
-func TestSecondHanAtMidnight(t *testing.T) {
+func TestSecondHandAtMidnight(t *testing.T) {
 	time := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
 
 	want := Point{X: 150, Y: 150 - 90}
@@ -19,4 +20,41 @@ func TestSecondHanAtMidnight(t *testing.T) {
 	if want != got {
 		t.Errorf("Got %v want %v", got, want)
 	}
+}
+
+func TestSecondHandAt30Seconds(t *testing.T) {
+	time := time.Date(1337, time.January, 1, 0, 30, 0, 0, time.UTC)
+
+	want := Point{X: 150, Y: 150 + 90}
+	got := SecondHand(time)
+
+	if want != got {
+		t.Errorf("Got %v want %v", got, want)
+	}
+}
+
+// write smaller tests first - comment out integration test during this
+func TestSecondsToRadians(t *testing.T) {
+	tests := []struct {
+		time time.Time
+		rad  float64
+	}{
+		// simpleTime(hour, min, second)
+		{simpleTime(0, 0, 30), math.Pi},
+		{simpleTime(0, 0, 0), 0},
+		{simpleTime(0, 0, 45), (math.Pi / 2) * 3},
+		{simpleTime(0, 0, 7), (math.Pi / 30) * 7},
+	}
+
+	for _, test := range tests {
+		want := test.rad
+		got := secondsInRadians(test.time)
+		if want != got {
+			t.Errorf("Want %v radians got %v", want, got)
+		}
+	}
+}
+
+func simpleTime(hour, min, second int) time.Time {
+	return time.Date(1337, time.January, 1, hour, min, second, 0, time.UTC)
 }
